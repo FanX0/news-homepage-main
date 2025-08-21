7145181b626d78a5b8d452872cd1bab5b71a32fb
fanx <faridazhari46@gmail.com>
2025-08-22 04:26:42 +0700

    fix aria

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("nav-toggle");
   const icon = document.getElementById("nav-side-img");
+  const menu = document.getElementById("site-nav");
+
   const OPEN = "./assets/images/icon-menu-close.svg";
   const CLOSED = "./assets/images/icon-menu.svg";
 
   btn.addEventListener("click", () => {
     const isOpen = btn.getAttribute("aria-expanded") === "true";
     btn.setAttribute("aria-expanded", String(!isOpen));
+    menu.hidden = isOpen;
     icon.src = isOpen ? CLOSED : OPEN;
     icon.style.width = isOpen ? "3rem" : "1.5rem";
   });
